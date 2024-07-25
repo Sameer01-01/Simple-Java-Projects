7774ae8d770a45b5117a6015ae423c5eb7aff702
Sameer Yadav <ysameer0026@gmail.com>
2024-07-26 01:09:00 +0530

    final commit

--- BankingApp.java
+++ BankingApp.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class BankingApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        bankaccount bank1 = new bankaccount("xyz","01");
+        bankaccount bank1 = new bankaccount("Sameer","3211492600878974");
         bank1.showmenu();
     } 
 }
