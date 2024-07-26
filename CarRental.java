d239687a532f19996c2f91471dd5b594adb1cd70
Sameer Yadav <ysameer0026@gmail.com>
2024-07-27 01:10:29 +0530

    added rental class

--- CarRental.java
+++ CarRental.java
@@ -60,6 +60,34 @@ class Customer{
     }
 }
 
+class Rental{
+    private Car car;
+    private Customer customer;
+    private int days;
+
+    public Rental(Car car, Customer customer, int days){
+        this.car=car;
+        this.customer=customer;
+        this.days=days;
+    }
+
+    public Car getcar(){
+        return car;
+    }
+
+    public Customer getcustomer(){
+        return customer;
+    }
+
+    public int getDays(){
+        return days;
+    }
+
+}
+
+
+
+
 public class CarRental {
     public static void main(String[] args) {
         Car car1 = new Car("1", "Toyota", "Corolla", 50.0);
